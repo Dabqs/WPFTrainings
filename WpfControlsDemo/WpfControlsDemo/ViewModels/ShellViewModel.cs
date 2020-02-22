d00674c958b07c074d431bfa0974e54bee55f8fc
mdapkus <dapkui.mantui@gmail.com>
2020-02-22 15:02:41 +0200

    added training for WPF combo box training.

---
+++ WpfControlsDemo/WpfControlsDemo/ViewModels/ShellViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Caliburn.Micro;
+using DemoLibrary;
+using DemoLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfControlsDemo.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+        public BindableCollection<PersonModel> People { get; set; }
+        private PersonModel selectedPerson;
+        private AddressModel selectedAddress;
+
+        public AddressModel SelectedAddress
+        {
+            get { return selectedAddress; }
+            set
+            {
+                selectedAddress = value;
+                SelectedPerson.PrimaryAddress = value;
+                NotifyOfPropertyChange(() => SelectedAddress);
+            }
+        }
+
+        public PersonModel SelectedPerson
+        {
+            get { return selectedPerson; }
+            set
+            {
+                selectedPerson = value;
+                SelectedAddress = value.PrimaryAddress;
+                NotifyOfPropertyChange(() => SelectedPerson);
+            }
+        }
+
+        public ShellViewModel()
+        {
+            DataAccess da = new DataAccess();
+            People = new BindableCollection<PersonModel>(da.GetPeople());
+        }
+    }
+}
